7021a5e94f9e153ac39d0232d38a6fdd863b0619
YuliiaDikun <dikun.yulia@gmail.com>
2023-06-05 17:56:03 +0300

    base routing

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+ <template>
+    <div>
+ <p>Products details for {{ id }}</p>
+ <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nulla!</p>
+    </div>
+ </template>
+ 
+ <script setup>
+ const { id } = useRoute().params
+ </script>
+ 
+ <style  scoped>
+ 
+ </style>
---
+++ pages/products/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+   <p>hello products</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
