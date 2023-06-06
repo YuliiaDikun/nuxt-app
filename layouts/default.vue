a542f0a6d835fd9928d6093300b3143f5f0a539d
YuliiaDikun <dikun.yulia@gmail.com>
2023-06-06 12:37:49 +0300

    feat: fetching data

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,13 @@
         <NuxtLink to="/" class="font-bold ">Nuxt App</NuxtLink>
         <ul class=" flex items-center justify-center gap-4 text-base font-semibold">
           <li>
-            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/" >Home</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/about">About</NuxtLink>
+            <NuxtLink to="/about" >About</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/products">Products</NuxtLink>
+            <NuxtLink to="/products" class="btn">Products</NuxtLink>
           </li>
         </ul>
       </nav>
@@ -26,5 +26,9 @@
 <style scoped>
 .router-link-exact-active {
   color: tomato;
+  background: transparent;
+  padding: 6px 12px;
+  border: 1px solid tomato;
+  border-radius: 10px;
 }
 </style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
-   <p>hello products</p>
+
+        <ul class="grid grid-cols-4 gap-5">
+            <li v-for="p in products">
+                <NuxtLink :to="`/products/${p.id}`">
+                    {{ p.title }}
+                </NuxtLink>
+            </li>
+        </ul>
+
     </div>
 </template>
 
 <script setup>
 definePageMeta({
-  layout: 'products'
+    layout: 'products'
 })
+
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
